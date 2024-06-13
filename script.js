c10d496afdb6f8dab299fedfc4bbb6245b83f5e6
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2024-06-13 22:43:05 +0200

    changing timing and adding a transition song
    Project Finish

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const img = document.getElementById('meme-img');
+    const transitionSound = document.getElementById('transition-sound');
     const sound = document.getElementById('sound');
     const newImgSrc = 'img/siu.jpeg';  
 
     img.addEventListener('click', function() {
-        // Ajoute la classe pour l'animation
         img.classList.add('spin');
 
-        // Change l'image après l'animation
+        transitionSound.play();
+
         setTimeout(function() {
             img.src = newImgSrc;
-            // Joue le son après le changement d'image
             sound.play();
 
-            // Recharger la page 3 secondes après le changement d'image et la lecture du son
             setTimeout(function() {
                 location.reload();
-            }, 3000); // 3 secondes avant de recharger la page
+            }, 1400); 
 
-            // Retire la classe d'animation pour permettre de cliquer à nouveau
+
             img.classList.remove('spin');
-        }, 2000);  // 2 secondes pour l'animation
+        }, 1800); 
     });
 });
